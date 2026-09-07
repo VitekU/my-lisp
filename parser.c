6be6cd96afad8456c5e96308d4ca60b2d8c11f63
VitekU <ungermann.vit@gmail.com>
2026-09-07 17:03:03 +0200

    (feat): add a parser

---
+++ parser.c
@@ -0,0 +1,102 @@
+#include "parser.h"
+#include "lexer.h"
+#include <ctype.h>
+#include <err.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+Node *create_number(int n) {
+    Node *node = malloc(sizeof(Node));
+    node->type = N_NUMBER;
+    node->number = n;
+    return node;
+
+}
+
+Node *create_string(char *s) {
+    Node *node = malloc(sizeof(Node));
+    node->type = N_STRING;
+    node->string = s;
+    return node;
+};
+Node *create_symbol(char *s) {
+    Node *node = malloc(sizeof(Node));
+    node->type = N_SYMBOL;
+    node->symbol = s;
+    return node;
+};
+Node *create_list() {
+    Node *node = malloc(sizeof(Node));
+    node->type = N_LIST;
+    node->list.count = 0;
+    node->list.capacity = INIT_LIST_CAPACITY;
+    node->list.elements = malloc(sizeof(Node) * INIT_LIST_CAPACITY);
+    return node;
+}
+
+void add_to_list(Node* list, Node* element) {
+    // reallocates the memory in case of the dynamic array being full
+    if (list->list.count >= list->list.capacity) {
+        list->list.capacity *= 2;
+        list->list.elements = realloc(list->list.elements, sizeof(Node) * list->list.capacity);
+    }
+
+    list->list.elements[list->list.count] = element;
+    list->list.count++;
+}
+
+void init_parser(Parser* parser, Lexer* lexer) {
+    parser->token = next_token(lexer);
+    parser->lexer = lexer;
+}
+
+void parser_next(Parser* parser) {
+    Token t = next_token(parser->lexer);
+    parser->token = t;
+}
+
+Token parser_curr(Parser* parser) {
+    return parser->token;
+}
+
+Node* parse_list(Parser* parser) {
+    Node* list = create_list();
+
+    // move past the L_PAREN token
+    parser_next(parser);
+
+    TokenType c_type = parser_curr(parser).type;
+
+    while (c_type != T_RPAREN && c_type != T_EOF) {
+        add_to_list(list, parse_expression(parser));
+    }
+
+    if (c_type != T_RPAREN) {
+        errx(1, "Syntax error, expected ')'\n");
+    }
+
+    // move past the R_PAREN token
+    parser_next(parser);
+    return list;
+}
+
+Node* parse_expression(Parser* parser) {
+    Token token = parser_curr(parser);
+
+    switch (token.type) {
+        case (T_NUMBER):
+            parser_next(parser);
+            return create_number(atoi(token.value));
+        case (T_STRING):
+            parser_next(parser);
+            return create_string(token.value);
+        case (T_SYMBOL):
+            parser_next(parser);
+            return create_symbol(token.value);
+        case (T_LPAREN):
+            return parse_list(parser);
+        default:
+            errx(1, "Unexpected token %s", token.value);
+    }
+}
---
+++ parser.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <stdio.h>
+#include "lexer.h"
+
+#define INIT_LIST_CAPACITY 4
+
+typedef enum {
+    N_NUMBER,
+    N_STRING,
+    N_SYMBOL,
+    N_LIST
+} NodeType;
+
+typedef struct Node {
+    NodeType type;
+    union {
+        int number;
+        char* string;
+        char* symbol;
+        struct {
+            struct Node **elements;
+            size_t count;
+            size_t capacity;
+        } list;
+    };
+} Node;
+
+typedef struct {
+    Token token;
+    Lexer *lexer;
+} Parser;
+
+void init_parser(Parser* parser, Lexer* lexer);
+void parser_next(Parser* parser);
+Token parser_curr(Parser* parser);
+
+Node* create_number(int n);
+Node* create_string(char* s);
+Node* create_symbol(char* s);
+Node* create_list();
+void add_to_list(Node* list, Node* element);
+
+Node* parse_expression(Parser* parser);
+Node* parse_list(Parser* parser);
